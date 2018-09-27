0130d8c59b78a7102fec45bf076785a86a7b4e9a
Akhil <akhilchandu108@gmail.com>
2018-09-27 17:54:19 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,14 +1,48 @@
 const path =  require('path');
+const http = require('http');
 const publicVarable = path.join(__dirname,'../public');
 const express = require('express');
+const socketIO =  require('socket.io');
+
 
 console.log(__dirname + '/public');
 
-const port = process.env.PORT || 3000;
-
 var app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicVarable));
 
-app.listen(port, ()=>{
-    console.log('server is up on '+ port);
+io.on('connection',(socket) =>{
+    console.log('user was connected..');
+    
+
+    socket.on('createMessage',(message)=>{
+       console.log('create Message',message);
+        
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createAt:new Date().getTime()
+        })
+
+    });
+    
+
+
+
+
+
+    // Event that i want to listen to is newEmail
+    
+
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected from servers');
+    });
+
 });
+
+server.listen(port, ()=>{
+    console.log('server is up on '+ port);
+});  
